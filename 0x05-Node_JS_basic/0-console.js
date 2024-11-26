541ee634fa8a6a783dc79ab2ef6cc0f4070f66be
Oluwafemi S.E. Ajanaku <111810384+Stanwukong@users.noreply.github.com>
2024-11-26 14:17:43 +0100

    Tasks 0 & 1

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Prints a message to stdout.
+ * @param {string} message - The message to be printed.
+ * @returns {void}
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
